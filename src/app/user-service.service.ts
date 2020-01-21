6a6aabcea32d687acbe6ceab9c587b4a3780b7cf
Anurag Gupta <anugup22@in.ibm.com>
2020-01-21 18:12:55 +0530

    Changing Name for service calling

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -4,24 +4,24 @@ import { User } from '../app/user';
 import { Observable } from 'rxjs/Observable';
 
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json',
-    'Authorization': 'Basic ' + btoa('user:user')
-  })
-};
+
 @Injectable()
 export class UserServiceService {
 
   private usersUrl: string;
 
- 
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + btoa('user:users')
+    })
+  };
  
   constructor(private http: HttpClient) {
     this.usersUrl = 'http://angularbackendservice/users';
   }
   public findAll(): Observable<User[]> {
-    return this.http.get<User[]>(this.usersUrl,httpOptions);
+    return this.http.get<User[]>(this.usersUrl,this.httpOptions);
   }
   public save(user: User) {
     return this.http.post<User>(this.usersUrl, user);
